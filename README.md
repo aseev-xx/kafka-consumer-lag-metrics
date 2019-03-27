eed49c983edfe10b9704cee5ad5946c1ede77907
Andrey Aseev <andreyaseev@msk-wifi-a1p3-caf-a_aseev-a.aseev.mail.msk>
2019-03-27 12:20:34 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# kafka-consumer-lag-metrics
+# Kafka consumer lag metrics
 Simple parser kafka consumer lag metrics from kafka-manager api and send to graphite
+
+## Getting started
+
+### Prerequisites
+Kafka-manager Routes: https://github.com/yahoo/kafka-manager/blob/master/conf/routes
---
+++ consumer_lag.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import requests
+import ConfigParser
+import time
+import socket
+
+# get base json object for next
+def get_json_object(url):
+
+   try:
+      obj = requests.get(url)
+   except requests.exceptions.RequestException as e:
+      print e
+      sys.exit(1)
+
+   return obj.json()
+
+# get all exist clusters for url concatenate in future
+def get_clusters(base_url):
+   obj = get_json_object(base_url + '/api/status/clusters')
+   clusters = []
+
+   for el in obj["clusters"]["active"]:
+      clusters.append(el["name"])
+
+   return clusters
+   
+def prepare_graphite_metrics(base_url,graphite_prefix):
+
+   clusters = get_clusters(base_url)
+
+   metrics = []
+
+   timestamp = int(time.time())
+
+   for cluster in clusters:
+      url = base_url + '/api/status/' + cluster + '/consumersSummary'   
+      obj = get_json_object(url)
+      consumers = obj["consumers"]
+
+      for consumer in consumers:
+	 for topic in consumer['topics']:
+	    value = graphite_prefix + cluster + '.' + consumer['name'] + '.' + topic
+	    message = '%s %s %d' % (value, consumer['lags'][topic], timestamp)
+	    metrics.append(message)
+
+   return metrics
+
+def send_graphite_metrics(message,graphite_host,graphite_port):
+   print 'sending message:\n%s' % message
+   sock = socket.socket()
+   sock.connect((graphite_host,graphite_port))
+   sock.sendall(message)
+   sock.close() 
+
+
+def main():
+
+   # store some static params from config
+   config = ConfigParser.SafeConfigParser()
+   config.read('consumer_lag.ini')
+
+   base_url = config.get('api','url')
+
+   bulk = prepare_graphite_metrics(base_url,config.get('graphite','prefix'))
+   message = '\n' . join(bulk) + '\n'
+
+   send_graphite_metrics(message,config.get('graphite','host'),int(config.get('graphite','port')))
+
+if __name__ == "__main__":
+   main()
